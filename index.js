a108a178532de68c1eab16aebe737f78c2a5c2c9
Julian Lam <julian@designcreateplay.com>
2015-03-23 12:24:48 -0400

    updated PR so that the header is added only to `/api/v1` level routes

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ var express = require('express'),
 	packages = require('./lib/packages'),
 	controllers = require('./lib/controllers');
 
-require('./lib/middleware')(app);
-
 require('./lib/routes')(app, controllers);
 
 winston.info('NodeBB Package Manager - Initializing');
