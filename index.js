1828da72de2a2899f5310a1c917f7af334a33750
Julian Lam <julian@nodebb.org>
2022-05-20 10:54:12 -0400

    refactor: eslint, rewriting a number of methods to async..await, support for scoped packages

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 'use strict';
-/* globals require */
 
-var express = require('express'),
-	bodyParser = require('body-parser'),
-	cronJob = require('cron').CronJob,
-	winston = require('winston'),
-	templates = require('templates.js'),
-	rdb = require('./lib/redis'),
-	app = express(),
-	packages = require('./lib/packages'),
-	analytics = require('./lib/analytics'),
-	controllers = require('./lib/controllers'),
+const express = require('express');
+const bodyParser = require('body-parser');
+const cronJob = require('cron').CronJob;
+const winston = require('winston');
+const templates = require('templates.js');
+const path = require('path');
 
-	requiredEnv = ['GITHUB_TOKEN', 'GITHUB_USER_AGENT'];
+const nconf = require('nconf');
 
-var nconf = require('nconf');
-var path = require('path');
-
-nconf.file({
+nconf.env().file({
 	file: path.join(__dirname, '/config.json'),
 });
 
-if (!requiredEnv.every(function(key) {
+const rdb = require('./lib/redis');
+
+const app = express();
+const packages = require('./lib/packages');
+const analytics = require('./lib/analytics');
+const controllers = require('./lib/controllers');
+
+const requiredEnv = ['GITHUB_TOKEN', 'GITHUB_USER_AGENT'];
+
+if (!requiredEnv.every((key) => {
 	if (process.env.hasOwnProperty(key)) {
 		nconf.set(key, process.env[key]);
 	}
@@ -29,17 +30,17 @@ if (!requiredEnv.every(function(key) {
 	return !!nconf.get(key);
 })) {
 	winston.error('[init] Required environment variables not found, please consult launch.template file');
-	return process.exit(1);
+	process.exit(1);
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 require('./lib/routes')(app, controllers);
 
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {
 	colorize: true,
-	level: process.env.NODE_ENV === 'production' ? 'info' : 'verbose'
+	level: process.env.NODE_ENV === 'production' ? 'info' : 'verbose',
 });
 
 winston.info('NodeBB Package Manager - Initializing');
@@ -61,19 +62,20 @@ app.listen(nconf.get('PORT') || 3000);
 winston.info('NodeBB Package Manager - Ready');
 
 // Check packaged sorted set. If missing, conduct initial sync
-packages.registry.init(function(err) {
+packages.registry.init((err) => {
 	if (err) {
 		winston.error('[init] Could not initialise registry.');
-		winston.error('[init] ' + err.message);
+		winston.error(`[init] ${err.message}`);
 		return process.exit(1);
 	}
 
-	rdb.zcard('packages', function(err, numPackages) {
+	// eslint-disable-next-line handle-callback-err
+	rdb.zcard('packages', (err, numPackages) => {
 		if (numPackages === 0) {
 			winston.info('[init] No packages detected in database, running initial sync');
 			packages.registry.sync(true);
 		} else {
-			winston.info('[init] Managing ' + numPackages + ' packages');
+			winston.info(`[init] Managing ${numPackages} packages`);
 		}
 	});
 });
